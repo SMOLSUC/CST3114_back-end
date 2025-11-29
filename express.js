1d1d983ba608a10aa931241bc4e427e893d0e480
SMOLSUC <fokxys@gmail.com>
2025-11-29 20:35:53 +0000

    added const fs

--- express.js
+++ express.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const fs = require('fs').promises; // Added for file system checks
 const { MongoClient, ObjectId } = require("mongodb"); 
 
 const app = express();
